35847245631e348dec00ae861392b08d5798d21e
evolshenanigans <gjgutierrez91@gmail.com>
2022-12-13 18:39:46 -0800

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/design/active-states.jpg 	new file:   src/design/desktop-design.jpg 	new file:   src/design/desktop-preview.jpg 	new file:   src/design/mobile-design.jpg 	new file:   src/images/favicon-32x32.png 	new file:   src/images/icon-cart.svg 	new file:   src/images/image-product-desktop.jpg 	new file:   src/images/image-product-mobile.jpg 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import img from "../src/images/image-product-desktop.jpg";
+import cart from '../src/images/icon-cart.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <article>
+      <picture>
+        <source media="(min-width: 616px)" src={img}/>
+        <img src={img} alt="" />
+      </picture>
+      <div className="card-main-content">
+        <span className="card-category">Perfume</span>
+        <h1 className="card-heading">Gabrielle Essence Eau De Parfum</h1>
+        <p className="card-description">
+          A floral, solar and voluptuous interpretation composed by Olivier Poige, Perfumer-Creator for the House of CHANEL
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="card-price-container">
+          <p className="card-price">149.99</p>
+          <del className="card-price"><span className="sr-only">Previous Price</span>169.99</del>
+        </div>
+        <button>
+          <img src={cart} alt="" aria-hidden="true" /> Add to Cart
+        </button>
+      </div>
+    </article>
   );
 }
 
